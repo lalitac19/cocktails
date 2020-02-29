24a9fc4155252208ce5533a01438c196b54579a7
Lalita Chopra <lalita@Lalitas-MBP.lan>
2020-02-29 15:16:08 +0000

    initial settings

---
+++ src/test/java/com/cocktails/CocktailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocktails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
